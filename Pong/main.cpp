0abbfaf27823d2d756680df1aea200b7728b290a
smiler <00riddler00@gmail.com>
2022-12-09 14:39:50 +0330

    vide mode and renamed to pong

---
+++ Pong/main.cpp
@@ -0,0 +1,30 @@
+﻿// Pong.cpp : Defines the entry point for the application.
+
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+	sf::RenderWindow window(sf::VideoMode(1280, 768), "Hello Window");
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+			if (event.type == sf::Event::KeyPressed) {
+				if (event.key.code == sf::Keyboard::Enter)
+					window.create(desktop, "Hello Window", sf::Style::Fullscreen);
+			}
+		}
+
+		window.clear();
+		window.display();
+	}
+	return 0;
+}
