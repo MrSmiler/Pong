44addda975068b93e97f0e50cdb94cae0c8924b6
smiler <00riddler00@gmail.com>
2022-12-09 20:52:34 +0330

    add clang formater

--- Pong/main.cpp
+++ Pong/main.cpp
@@ -1,30 +1,37 @@
-﻿// Pong.cpp : Defines the entry point for the application.
+﻿// Copyright 2022 MrSmiler
 
-
-#include <iostream>
 #include <SFML/Graphics.hpp>
+#include <iostream>
 #include <vector>
 
-using namespace std;
 
 int main()
 {
-	sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
-	sf::RenderWindow window(sf::VideoMode(1280, 768), "Hello Window");
-	while (window.isOpen()) {
-		sf::Event event;
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed) {
-				window.close();
-			}
-			if (event.type == sf::Event::KeyPressed) {
-				if (event.key.code == sf::Keyboard::Enter)
-					window.create(desktop, "Hello Window", sf::Style::Fullscreen);
-			}
-		}
+    sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+    sf::RenderWindow window(sf::VideoMode(1280, 768), "Hello Window");
+    while (window.isOpen())
+    {
+        sf::Event event;
 
-		window.clear();
-		window.display();
-	}
-	return 0;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+
+            if (event.type == sf::Event::KeyPressed)
+            {
+                if (event.key.code == sf::Keyboard::Enter)
+                {
+                    window.create(desktop, "Hello Window",
+                                  sf::Style::Fullscreen);
+                }
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
+    return 0;
 }
